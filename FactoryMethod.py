9fa5a3a7ed3593f3f8dd78f74fe71129666b5add
Faezeh <fmadadi69@gmail.com>
2024-03-27 14:20:57 +0330

    Factory Method Design Pattern is refactored.

--- FactoryMethod.py
+++ FactoryMethod.py
@@ -1,43 +1,99 @@
-class Interviewer:
-    def makeInterview(self):
+from abc import ABC, abstractmethod
+
+
+class Character(ABC):
+    def __init__(self, name, health):
+        self.name = name
+        self.health = health
+
+    @abstractmethod
+    def attack(self):
         pass
 
 
-class Developer(Interviewer):
-    def makeInterview(self):
-        print(f"Interview is done by a developer")
+class Warrior(Character):
+    def __init__(self, name, health, strength):
+        super().__init__(name, health)
+        self.strength = strength
+
+    def attack(self):
+        print(f"""
+                Warrior name: {self.name}
+                health level: {self.health}
+                strength: {self.strength}
+                Attacks with: Sword
+                """)
 
 
-class CommunityExecutive(Interviewer):
-    def makeInterview(self):
-        print(f"Interview is done by a community executive")
+class Mage(Character):
+    def __init__(self, name, health, mana):
+        super().__init__(name, health)
+        self.mana = mana
+
+    def attack(self):
+        print(f"""
+                Mage name: {self.name}
+                health level: {self.health}
+                mana: {self.mana}
+                Attacks with: Spell
+                """)
 
 
-class HiringManager:
-    def chooseInterviewer(self):
+class Archer(Character):
+    def __init__(self, name, health, accuracy):
+        super().__init__(name, health)
+        self.accuracy = accuracy
+
+    def attack(self):
+        print(f"""
+                Archer name: {self.name}
+                health level: {self.health}
+                accuracy: {self.accuracy}
+                Attacks with: Arrows
+                """)
+
+# ########### FACTORY METHOD ############# #
+
+
+class CharacterCreator(ABC):
+    @abstractmethod
+    def create_character(self, name, health):
         pass
 
-    def takeInterview(self):
 
-        interviewer = self.chooseInterviewer()
-        interviewer.makeInterview()
-
-
-class DevelopmentManager(HiringManager):
-    def chooseInterviewer(self):
-        return Developer()
+class WarriorCreator(CharacterCreator):
+    def create_character(self, name, health):
+        return Warrior(name, health, strength=20)
 
 
-class MarketingManager(HiringManager):
-    def chooseInterviewer(self):
-        return CommunityExecutive()
+class MageCreator(CharacterCreator):
+    def create_character(self, name, health):
+        return Mage(name, health, mana=10)
 
 
-devManager = DevelopmentManager()
-devManager.takeInterview()
+class ArcherCreator(CharacterCreator):
+    def create_character(self, name, health):
+        return Archer(name, health, accuracy=100)
 
-marketingManager = MarketingManager()
-marketingManager.takeInterview()
+
+# ############# CLIENT CODE ############## #
+player_name = str(input("Enter your character's name:"))
+player_health = int(input("Enter your character's health:"))
+player_choice = str(input("Choose your character's type (Warrior - Mage - Archer):"))
+creator = None
+
+if player_choice == 'Warrior':
+    creator = WarriorCreator()
+elif player_choice == 'Mage':
+    creator = MageCreator()
+elif player_choice == 'Archer':
+    creator = ArcherCreator()
+else:
+    player_choice = str(input("Invalid Character type. Choose your character's type (Warrior - Mage - Archer) AGAIN:"))
+
+if creator:
+    character = creator.create_character(player_name, player_health)
+    character.attack()
 
 
 
