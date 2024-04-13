42aa8dbf729ce7fc3231939e348739a5fa576756
Faezeh <fmadadi69@gmail.com>
2024-04-13 13:18:47 +0330

    Abstract Factory is refactored.

--- AbstractFactory.py
+++ AbstractFactory.py
@@ -32,35 +32,48 @@ class Carpenter(DoorFittingExpert):
 
 
 class DoorFactory:
-    def makeDoor(self):
+    def makeDoor(self) -> Door:
         pass
-    def makeFittingExpert(self):
+
+    def makeFittingExpert(self) -> DoorFittingExpert:
         pass
 
 
 class WoodenDoorFactory(DoorFactory):
-    def makeDoor(self):
+    def makeDoor(self) -> Door:
         return WoodenDoor()
 
-    def makeFittingExpert(self):
+    def makeFittingExpert(self) -> DoorFittingExpert:
         return Carpenter()
 
 
 class IronDoorFactory(DoorFactory):
-    def makeDoor(self):
+    def makeDoor(self) -> Door:
         return IronDoor()
 
-    def makeFittingExpert(self):
+    def makeFittingExpert(self) -> DoorFittingExpert:
         return Welder()
 
 
-woodenDoor = WoodenDoorFactory()
-door = woodenDoor.makeDoor()
-door.getDescription()
+class Application:
+    def __init__(self, factory: DoorFactory):
+        self.factory = factory
 
-expert = woodenDoor.makeFittingExpert()
-expert.getDescription()
+    def create_Door(self):
+        door = self.factory.makeDoor()
+        fiiting_expert = self.factory.makeFittingExpert()
 
+        door.getDescription()
+        fiiting_expert.getDescription()
+
+
+wooden_door = WoodenDoorFactory()
+app1 = Application(wooden_door)
+app1.create_Door()
+
+iron_door = IronDoorFactory()
+app2 = Application(iron_door)
+app2.create_Door()
 
 # ################ THEMES ################## #
 class Button(ABC):
@@ -97,27 +110,27 @@ class DarkCheckbox(CheckBox):
 
 class GUIFactory(ABC):
     @abstractmethod
-    def create_button(self):
+    def create_button(self) -> Button:
         pass
 
     @abstractmethod
-    def create_checkbox(self):
+    def create_checkbox(self) -> CheckBox:
         pass
 
 
 class LightTheme(GUIFactory):
-    def create_button(self):
+    def create_button(self) -> Button:
         return LightButton()
 
-    def create_checkbox(self):
+    def create_checkbox(self) -> CheckBox:
         return LightCheckBox()
 
 
 class DarkTheme(GUIFactory):
-    def create_button(self):
+    def create_button(self) -> Button:
         return DarkButton()
 
-    def create_checkbox(self):
+    def create_checkbox(self) -> CheckBox :
         return DarkCheckbox()
 
 
@@ -126,7 +139,18 @@ class Application:
         self.factory = factory
 
     def create_gui(self):
-        self.factory.create_button()
-        self.factory.create_checkbox()
+        button = self.factory.create_button()
+        checkbox = self.factory.create_checkbox()
+        button.draw()
+        checkbox.draw()
+
+
+light_theme = LightTheme()
+app1 = Application(light_theme)
+app1.create_gui()
+
+dark_theme = DarkTheme()
+app2 = Application(dark_theme)
+app2.create_gui()
 
 
